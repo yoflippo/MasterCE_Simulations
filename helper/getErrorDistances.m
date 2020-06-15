19ed5b8d87f5a574208a7610d38277ba919a6293
Mark Schrauwen <yoflippo@gmail.com>
2020-06-15 15:29:32 +0200

    working code after attempt to add e_{cdis} see report

--- helper/getErrorDistances.m
+++ helper/getErrorDistances.m
@@ -1,6 +1,6 @@
-function error = getErrorDistances(anchors,PosClean,PosDirty)
+function error = getErrorDistances(anchors,clean,dirty)
 nA = size(anchors,1);
-nT = length(PosClean);
-difference = dis(anchors,PosClean)-dis(anchors,PosDirty);
-error = round((sum(difference(:).^2)/(nA*nT)),1);-am
+nT = length(clean);
+difference = dis(anchors,clean)-dis(anchors,dirty);
+error = round((sum(difference(:).^2)/(nA*nT)),1);
 end
