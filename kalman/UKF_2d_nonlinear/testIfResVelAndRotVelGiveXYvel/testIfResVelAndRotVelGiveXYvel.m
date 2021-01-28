9eeff7767c70c69e895b7bed4234429a8fd122b9
yoflippo <yoflippo@gmail.com>
2021-01-28 13:46:55 +0100

    small changes in presentation of output

--- kalman/UKF_2d_nonlinear/testIfResVelAndRotVelGiveXYvel/testIfResVelAndRotVelGiveXYvel.m
+++ kalman/UKF_2d_nonlinear/testIfResVelAndRotVelGiveXYvel/testIfResVelAndRotVelGiveXYvel.m
@@ -6,26 +6,32 @@ close all; clear variables; clc;
 cd(ap.thisFile);
 load(fullfile(fileparts(ap.thisFile),'UKF_create_simulation_data.mat'))
 
+
 figure('units','normalized','outerposition',[0.1 0.1 0.9 0.9]); nsp = 1;
 subplot(3,3,nsp); nsp = nsp + 1;
 plot(clean.velocity.res,'DisplayName','clean resultant velocity');
 grid on; grid minor; title('Resultant velocity');
 
+
 subplot(3,3,nsp); nsp = nsp + 1;
 plot(clean.velocity.angularRate,'DisplayName','clean angular velocity');
 grid on; grid minor; title('Angular velocity');
+
 
 subplot(3,3,nsp); nsp = nsp + 1;
 plot(gradient(position.x2,temporalspecs.dt2),'DisplayName','velocity x');
 grid on; grid minor; title('Desired velocity x');
 
+
 subplot(3,3,nsp); nsp = nsp + 1;
 plot(gradient(position.y2,temporalspecs.dt2),'DisplayName','velocity y');
 grid on; grid minor; title('Desired velocity y');
 
+
 subplot(3,3,nsp); nsp = nsp + 1;
 plot(clean.velocity.angles,'DisplayName','clean angular velocity');
 grid on; grid minor; title('angles clean');
+
 
 subplot(3,3,nsp); nsp = nsp + 1;
 startAngle = atan2d((position.y2(2)-position.y2(1)),(position.x2(2)-position.x2(1)));
@@ -34,17 +40,18 @@ plot(Angles,'DisplayName','Angle','Color','m');
 grid on; grid minor; title('Angles');
 
 
-
 subplot(3,3,nsp); nsp = nsp + 1;
 V = clean.velocity.res;
 velx = V.*cosd(Angles);
 plot(velx,'DisplayName','velocity x','Color','g');
 grid on; grid minor; title('Attempt to get velocity x');
 
+
 subplot(3,3,nsp); nsp = nsp + 1;
 vely = V.*sind(Angles);
 plot(vely,'DisplayName','velocity y','Color','r');
 grid on; grid minor; title('Attempt to get velocity y');
+
 
 subplot(3,3,nsp); nsp = nsp + 1;
 plot(position.x2,position.y2);
